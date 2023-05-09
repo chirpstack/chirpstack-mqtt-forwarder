472a2739e64e6b62d972d0c1504b87c88a49ec26
Orne Brocaar <info@brocaar.com>
2023-05-09 15:34:32 +0100

    Add rust-toolchain.toml + shell.nix files.
    This makes it easier to start a devshell without using Docker.

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-22.11.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.cacert
+    pkgs.rustup
+    pkgs.protobuf
+    pkgs.perl
+    pkgs.cmake
+    pkgs.clang
+  ];
+  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+  BINDGEN_EXTRA_CLANG_ARGS = "-I${pkgs.llvmPackages.libclang.lib}/lib/clang/${pkgs.llvmPackages.libclang.version}/include";
+}
